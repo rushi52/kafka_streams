08e641b957a5a910fb7e837c3da2e5e2da32a966
rushi52 <rushi.jammana@gmail.com>
2024-09-16 08:37:15 +0530

    Redis configuration

--- src/main/java/com/example/kafkastream/controller/KafkaController.java
+++ src/main/java/com/example/kafkastream/controller/KafkaController.java
@@ -1,9 +1,12 @@
 
 package com.example.kafkastream.controller;
 import com.example.kafkastream.model.Order;
+//import com.example.kafkastream.service.HitService;
 import com.example.kafkastream.service.KafkaProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +20,38 @@ public class KafkaController {
     @Autowired
     private KafkaProducerService kafkaProducerService;
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+//    @Autowired
+//    HitService hitService;
+
 //    @PostMapping("/publish")
 //    public String publishMessage(@RequestBody Order order) {
 //        kafkaProducerService.sendMessage("input-topic", order);
 //        return "Message published: " + order;
 //    }
 
+//    @GetMapping("redis/getOrder")
+//    public int getHitCount(){
+//        return hitService.getHitCount();
+//    }
+//
+//    @GetMapping("redis/incrementOrder")
+//    public Long incrementOrder(){
+//        return hitService.incrementHits();
+//    }
+
     @PostMapping("/orders")
     public String createOrder(@RequestBody Order order) {
-        kafkaTemplate.send("input-orders", order.getId(), order);
-        return "Order sent to Kafka!";
+        if(redisTemplate.opsForValue().get(order.getId())!=null){
+            System.out.println("Cached in redis"+order);
+            return order.getId();
+        }
+        else {
+            kafkaTemplate.send("input-orders", order.getId(), order);
+            return "Order sent to Kafka!";
+        }
     }
 }
--- src/main/java/com/example/kafkastream/service/KafkaMessageListener.java
+++ src/main/java/com/example/kafkastream/service/KafkaMessageListener.java
@@ -1,13 +1,19 @@
 package com.example.kafkastream.service;
 import com.example.kafkastream.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaMessageListener {
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @KafkaListener(topics = "output-orders", groupId = "streams-group")
     public void listen(Order order) {
+        redisTemplate.opsForValue().set(order.getId(), order);
         System.out.println("Received message from output-topic: " + order);
     }
 }
